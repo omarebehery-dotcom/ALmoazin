aa3c7804186eaae77a65f5ec7fb5f6f49d233529
omarebehery-dotcom <omar.ebehery@gmail.com>
2026-07-01 13:28:11 -0700

    Update qibla.js

--- qibla.js
+++ qibla.js
@@ -1,1 +1,9 @@
+async function loadQibla() {
+  const res = await fetch("https://api.aladhan.com/v1/qibla/30.0444/31.2357");
+  const data = await res.json();
 
+  document.getElementById("qibla").textContent =
+    "اتجاه القبلة: " + Math.round(data.data.direction) + "°";
+}
+
+loadQibla();
