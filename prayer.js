58870a5292010d2d806a2840ad769899b2c9ce6d
omarebehery-dotcom <omar.ebehery@gmail.com>
2026-07-01 13:27:20 -0700

    Update prayer.js

--- prayer.js
+++ prayer.js
@@ -1,1 +1,5 @@
-
+async function getPrayerTimes() {
+  const res = await fetch("https://api.aladhan.com/v1/timingsByCity?city=Cairo&country=EG");
+  const data = await res.json();
+  return data.data.timings;
+}
