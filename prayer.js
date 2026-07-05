e6576f1bdb99b428b128c90be31675f03c4f5f2d
omarebehery-dotcom <omar.ebehery@gmail.com>
2026-07-05 23:37:29 +0300

    Delete prayer.js

--- prayer.js
+++
@@ -1,5 +0,0 @@
-async function getPrayerTimes() {
-  const res = await fetch("https://api.aladhan.com/v1/timingsByCity?city=Cairo&country=EG");
-  const data = await res.json();
-  return data.data.timings;
-}
