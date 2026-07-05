ef8873f9f3782e5efa0dced0629dcb4882453f16
omarebehery-dotcom <omar.ebehery@gmail.com>
2026-07-05 23:37:43 +0300

    Delete qibla.js

--- qibla.js
+++
@@ -1,9 +0,0 @@
-async function loadQibla() {
-  const res = await fetch("https://api.aladhan.com/v1/qibla/30.0444/31.2357");
-  const data = await res.json();
-
-  document.getElementById("qibla").textContent =
-    "اتجاه القبلة: " + Math.round(data.data.direction) + "°";
-}
-
-loadQibla();
